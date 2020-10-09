6ea179d31c45bad9badce907b5927154a6854667
Prasanna Hebbar <prasanna.hebbar1998@gmail.com>
2020-10-09 11:13:52 -0400

    demo app

---
+++ models/user_model.js
@@ -0,0 +1,26 @@
+const { Model } = require('objection');
+const knex = require('../knex')
+
+Model.knex(knex)
+
+class User extends Model {
+  static get tableName() {
+    return 'users';
+  }
+
+  static get relationMappings() {
+      const Message = require('./Message')
+      return {
+          messages: {
+              relation: Model.HasManyRelation,
+              modelClass: Message,
+              join: {
+                  from: 'users.id',
+                  to: 'messages.user_id'
+              }
+          }
+      }
+  }
+}
+
+module.exports = User;
---
+++ seeds/users.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'fodark', email: 'mock@email.com'},
+        {username: 'john', email: 'mock2@email.com'},
+        {username: 'david', email: 'mock3@email.com'}
+      ]);
+    });
+};
+
+
